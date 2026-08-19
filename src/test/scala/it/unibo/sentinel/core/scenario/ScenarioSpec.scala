0ab3ed8b0543f525bb3d1c05641c9084f3e7b3b3
LeonardoMarcaccio <leonardo.marcaccio@studio.unibo.it>
2026-08-19 11:43:47 +0200

    feat: selector

--- src/test/scala/it/unibo/sentinel/core/scenario/ScenarioSpec.scala
+++ src/test/scala/it/unibo/sentinel/core/scenario/ScenarioSpec.scala
@@ -32,6 +32,9 @@ class ScenarioSpec extends UnitTest:
       "have a default routing policy" in:
         s0.routing shouldBe Policies.Routing.Distance
 
+      "have a default assignment policy" in:
+        s0.assignment shouldBe Policies.Assignment.Nearest
+
     "place a robot" should:
 
       "return a new scenario with the robot placed" in:
@@ -71,7 +74,7 @@ class ScenarioSpec extends UnitTest:
       "return a new scenario with the mission added" in:
         val mission = Mission(
           id = MissionId("M1"),
-          task = Task.Move(Position(1, 1)),
+          task = Task.goto(Position(1, 1)),
           duration = 10
         )
         val result = s0.load(mission).right.value
@@ -80,7 +83,7 @@ class ScenarioSpec extends UnitTest:
       "signal that the mission id already exists" in:
         val mission = Mission(
           id = MissionId("M1"),
-          task = Task.Move(Position(1, 1)),
+          task = Task.goto(Position(1, 1)),
           duration = 10
         )
         val result =
@@ -96,3 +99,10 @@ class ScenarioSpec extends UnitTest:
         val newRouting = Mockito.mock[Policies.Routing]()
         val result = s0.withRouting(newRouting)
         result.routing shouldBe newRouting
+
+    "change the assignment policy" should:
+
+      "return a new scenario with the assignment policy changed" in:
+        val newAssignment = Mockito.mock[Policies.Assignment]()
+        val result = s0.withAssignment(newAssignment)
+        result.assignment shouldBe newAssignment
