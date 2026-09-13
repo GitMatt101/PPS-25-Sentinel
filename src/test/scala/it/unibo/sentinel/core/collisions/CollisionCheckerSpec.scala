1d0d44434a7ecb8ca542e2cb790f48ee50f49356
Mattia Forti <mattia.forti2@studio.unibo.it>
2026-09-13 12:23:03 +0200

    test: made CollisionChecker tests more clear

--- src/test/scala/it/unibo/sentinel/core/collisions/CollisionCheckerSpec.scala
+++ src/test/scala/it/unibo/sentinel/core/collisions/CollisionCheckerSpec.scala
@@ -1,7 +1,6 @@
 package it.unibo.sentinel.core.collisions
 
 import it.unibo.sentinel.UnitTest
-import it.unibo.sentinel.core.mission.{Mission, MissionId, Priority}
 import it.unibo.sentinel.core.robot.{Robot, RobotId}
 import it.unibo.sentinel.core.routing.{Path, Step}
 import it.unibo.sentinel.core.scenario.Placement
@@ -12,24 +11,23 @@ import it.unibo.sentinel.core.scenario.Intent
 trait CollisionCheckerFixture:
   self: UnitTest =>
 
-  val mission: Mission =
-    Mission.relocate(MissionId("M"), Position(1, 1), Tick(10), Priority.normal)
-
   val r1: Robot = Robot.drone(RobotId("R1"))
   val r2: Robot = Robot.drone(RobotId("R2"))
   val r3: Robot = Robot.drone(RobotId("R3"))
   val r4: Robot = Robot.drone(RobotId("R4"))
   val r5: Robot = Robot.drone(RobotId("R5"))
 
-  val group1: Seq[Robot] = Seq(r1, r2, r3)
-
-  val p4 = Placement(r4, Position(4, 4))
-  val p5 = Placement(r5, Position(5, 5))
-  r4.follow(Path(Step(Position(5, 5), Tick.unit)))
-  r5.follow(Path(Step(Position(4, 4), Tick.unit)))
+  val p1 = Placement(r1, Position(0, 0))
+  val p2 = Placement(r2, Position(1, 0))
+  val p3 = Placement(r3, Position(3, 3))
+  val p4 = Placement(r4, Position(5, 3))
+  val p5 = Placement(r5, Position(4, 4))
 
   protected def toIntent(placement: Placement): Intent =
-    Intent(placement.robot.id, placement.at, placement.next, Some(mission))
+    Intent(placement.robot.id, placement.at, placement.next, None)
+
+  protected def singleStep(target: Position): Path =
+    Path(Step(target, Tick.zero))
 
 class CollisionCheckerSpec extends UnitTest with CollisionCheckerFixture:
 
@@ -38,35 +36,33 @@ class CollisionCheckerSpec extends UnitTest with CollisionCheckerFixture:
     "checking direct collisions" should:
 
       "return a list of robots that are colliding face to face" in:
-        r4.tick()
-        r5.tick()
-        val intents = Seq(toIntent(p4), toIntent(p5))
-        CollisionChecker.directCollisions(
-          intents
-        ) should contain theSameElementsAs Seq(
-          DirectCollision(r4.id, r5.id)
-        )
+        r1.follow(singleStep(Position(1, 0)))
+        r2.follow(singleStep(Position(0, 0)))
+        val intents = Seq(toIntent(p1), toIntent(p2))
+        val collisions = CollisionChecker.directCollisions(intents)
+        collisions shouldBe Seq(DirectCollision(r1.id, r2.id))
 
     "checking indirect collisions" should:
 
-      "return a list of groups of robots that collide" in:
-        val target = Position(1, 1)
-        val intents =
-          group1.map(r => Intent(r.id, Position(0, 0), target, Some(mission)))
-        CollisionChecker.indirectCollisions(
-          intents
-        ) should contain theSameElementsAs Seq(
-          IndirectCollision(target, group1.map(_.id))
-        )
+      "return a list of groups of robots that (will) collide" in:
+        val target = Position(4, 3)
+        val group = Seq(p3, p4, p5)
+        group.foreach(_.robot.follow(singleStep(target)))
+        val intents = group.map(toIntent)
+        val collisions = CollisionChecker.indirectCollisions(intents)
+        val expected = Seq(IndirectCollision(target, group.map(_.robot.id)))
+        collisions shouldBe expected
 
-      "not signal any collision between two robots moving to different positions" in:
-        val intentA =
-          Intent(RobotId("RA"), Position(0, 0), Position(1, 0), Some(mission))
-        val intentB =
-          Intent(RobotId("RB"), Position(2, 0), Position(2, 0), Some(mission))
-        val collisions =
-          CollisionChecker.indirectCollisions(Seq(intentA, intentB))
-        collisions should contain theSameElementsAs Seq(
-          IndirectCollision(Position(1, 0), Seq(RobotId("RA"))),
-          IndirectCollision(Position(2, 0), Seq(RobotId("RB")))
+    "there are no collisions" should:
+
+      "not signal any type of collision between two robots moving to different positions" in:
+        r1.follow(singleStep(Position(0, 1)))
+        r2.follow(singleStep(Position(1, 1)))
+        val notColliding = Seq(toIntent(p1), toIntent(p2))
+        val indirect = CollisionChecker.indirectCollisions(notColliding)
+        val direct = CollisionChecker.directCollisions(notColliding)
+        indirect shouldBe Seq(
+          IndirectCollision(p1.next, Seq(p1.robot.id)),
+          IndirectCollision(p2.next, Seq(p2.robot.id))
         )
+        direct shouldBe empty
